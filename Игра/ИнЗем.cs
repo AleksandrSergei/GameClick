a6f6ffda9af94a101809d492b7d9cc31b96ff578
Александр Погудин <stud0000242629@study.utmn.ru>
2022-05-06 08:51:53 +0500

    Начал делать форму ИнЗем (Sergei)

---
+++ Игра/ИнЗем.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Игра
+{
+    public partial class ИнЗем : Form
+    {
+        private int book = 0;
+        private int sheet = 0;
+        private int pencil = 0;
+        private int brain = 0;
+        
+        public ИнЗем()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
