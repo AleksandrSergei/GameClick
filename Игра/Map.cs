d0d90d452ce6a1205126137cb1a8d79e05a0d66a
Sergei <sergej.lagunov2016@yandex.ru>
2022-06-03 10:32:32 +0500

    Поменял путь на форме Map

--- Игра/Map.cs
+++ Игра/Map.cs
@@ -22,6 +22,20 @@ namespace Игра
             label1.Text = Statistics.number_clicks.ToString();
             pictureBox3.Image = Image.FromFile(Button_image_map.Image_IMiKN);
             pictureBox3.Enabled = Button_image_map.Image_IMiKN_Enabled;
+            pictureBox2.Image = Image.FromFile(Button_image_map.Image_IGiP);
+            pictureBox2.Enabled = Button_image_map.Image_IGiP_Enabled;
+            pictureBox4.Image = Image.FromFile(Button_image_map.Image_InBio);
+            pictureBox4.Enabled = Button_image_map.Image_InBio_Enabled;
+            pictureBox5.Image = Image.FromFile(Button_image_map.Image_InZem);
+            pictureBox5.Enabled = Button_image_map.Image_InZem_Enabled;
+            pictureBox7.Image = Image.FromFile(Button_image_map.Image_IPiP);
+            pictureBox7.Enabled = Button_image_map.Image_IPiP_Enabled;
+            pictureBox8.Image = Image.FromFile(Button_image_map.Image_IFK);
+            pictureBox8.Enabled = Button_image_map.Image_IFK_Enabled;
+            pictureBox10.Image = Image.FromFile(Button_image_map.Image_FizTech);
+            pictureBox10.Enabled = Button_image_map.Image_FizTech_Enabled;
+            pictureBox11.Image = Image.FromFile(Button_image_map.Image_FEI);
+            pictureBox11.Enabled = Button_image_map.Image_FEI_Enabled;
         }
 
         void Form1_Load(object sender, EventArgs e)
